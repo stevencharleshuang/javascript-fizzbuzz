85ad8d65916a9d4565d4c5edf96d4cf12d072a7d
Steven Huang <steven.charles.huang@gmail.com>
2019-09-18 16:15:53 -0400

    completed fizzBuzz

---
+++ fizzbuzz.js
@@ -0,0 +1,15 @@
+const fizzBuzz = (start = 1, end = 101) => {
+  for (let i = start; i <= end; i += 1) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log('fizzbuzz');
+    } else if (i % 3 === 0) {
+      console.log('fizz');
+    } else if (i % 5 === 0) {
+      console.log('buzz');
+    } else {
+      console.log(i);
+    }
+  }
+}
+
+fizzBuzz();
