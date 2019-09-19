701ca863320a90fbbc4ec024847753e776bdf83b
Steven Huang <steven.charles.huang@gmail.com>
2019-09-19 08:48:57 -0400

    added code description for function

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,3 +1,12 @@
+/**
+ * Given a starting number and ending number of a range, prints the numbers of the range. 
+ * If a number is divisible by 3, prints "fizz".
+ * If a number is divisible by 5, prints "buzz".
+ * If a number is divisible by 3 and 5, prints "fizzbuzz".
+ * If no arguments are passed to the function, default values are used.
+ * @param { number } start: The number that the function will begin from
+ * @param { number } end: The number that the function should arrive at 
+ */
 const fizzBuzz = (start = 1, end = 101) => {
   for (let i = start; i <= end; i += 1) {
     if (i % 3 === 0 && i % 5 === 0) {
